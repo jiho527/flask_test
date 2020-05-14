55c0f09efb6d7e4372fe67ac37eb6248dfe816a8
Jiho Lim <very725@naver.com>
2020-05-14 18:43:14 +0900

    add comment and change host

--- upload.py
+++ upload.py
@@ -1,19 +1,21 @@
 from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
+# Flask 모듈 임포트
+from werkzeug.utils import secure_filename # 파일이름의 보안 버전 반환
 
-app = Flask(__name__)
+app = Flask(__name__) # Flask 객체를 app에 할당
 
-@app.route('/upload')
-def load_file():
+@app.route('/') # app객체를 이용해 라우팅 경로를 결정
+def load_file(): # 해당 라우팅 경로로 요청이 올 때 실행할 함수를 바로 밑에 작성
    return render_template('upload.html')
 	
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
-      f.save("./uploads/" + secure_filename(f.filename))
-      #f.save(secure_filename(f.filename)) // upload.py 디렉토리에 저
+      f.save("./uploads/" + secure_filename(f.filename)) # uploads 폴더에 사진 저장
+      #f.save(secure_filename(f.filename)) // upload.py가 있는 디렉토리에 저장
       return 'file uploaded successfully'
-		
+
 if __name__ == '__main__':
-   app.run(debug = True)
+   #app.run(debug = True)
+   app.run(host="172.30.1.21", port="8080") # upload.html 파일의 form action도 바꿔줘야 함
